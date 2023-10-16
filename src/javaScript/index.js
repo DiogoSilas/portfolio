69751000d2f8051724f2a59245c317c996bca96b
Diogo Silas <diogosilas4@gmail.com>
2023-10-16 18:59:18 -0300

    Edição da seção Contatos; No icone do menu; E fontes e posições dos textos e títulos.

--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -2,7 +2,9 @@ function mobileMenu() {
     let menuMobile = document.querySelector('.mobile-menu')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
+        document.querySelector('.icone').src = "src/images/icone-menu-hamburguer.png"
     } else {
         menuMobile.classList.add('open')
+        document.querySelector('.icone').src = "src/images/icone-menu-x.png"
     }
 }
