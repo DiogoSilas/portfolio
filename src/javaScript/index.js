4629f78b7308f54ebe9cfd97440e79e8631d770a
Diogo Silas <diogosilas4@gmail.com>
2023-11-03 17:44:00 -0300

    Editando Portfólio

--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -2,9 +2,7 @@ function mobileMenu() {
     let menuMobile = document.querySelector('.mobile-menu')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
-        document.querySelector('.icone').src = "src/images/icone-menu-hamburguer.png"
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icone').src = "src/images/icone-menu-x.png"
     }
 }
